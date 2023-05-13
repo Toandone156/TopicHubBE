49eb0988f0c4b16dde6c8dc732e675e41f1a7af7
Toandone <toanptde160263@fpt.edu.vn>
2023-05-13 15:54:06 +0700

    Add model

---
+++ Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TopicHubBE.Models
+{
+    public class Category
+    {
+        //Attributes
+        [Key]
+        public int CategoryID { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string CategoryName { get; set; }
+
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TopicHubBE.Models
+{
+    public class User
+    {
+        //Attributes
+        [Key]
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string HashPassword { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string FullName { get; set; }
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [DataType(DataType.ImageUrl)]
+        public string Avatar { get; set; }
+        [DefaultValue(0)]
+        public int RatingScore { get; set; }
+        [DefaultValue(0)]
+        [DataType(DataType.Currency)]
+        public int InAppMoney { get; set; }
+        public string? BankAccount { get; set; }
+
+        //Relations
+        //public virtual ICollection<Hub> Hubs { get; set;}
+    }
+}
